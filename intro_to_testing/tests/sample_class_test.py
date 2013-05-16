4fdee961fdff524447db1aa4157eaf8043764f70
Laura Uva <laura@cpusage.com>
2013-05-15 20:15:44 -0700

    Wrote unit tests for each method in sample class

--- intro_to_testing/tests/sample_class_test.py
+++ intro_to_testing/tests/sample_class_test.py
@@ -16,15 +16,19 @@ def test_sample_class_mylist():
 
 def test_myname():
     """ Verify that we return myname """
-    raise SkipTest
-    pass
+    expected_name = 'Laura'
+    s = SampleClass()
+    assert_equal(s.myname(), expected_name)
 
 def test_add_myname_to_mylist():
     """ Verify that we can add myname to mylist """
-    raise SkipTest
-    pass
+    # raise SkipTest
+    expected_list = [ 'one', 'two', 'three', 'four', 'five', 'Laura']
+    s = SampleClass()
+    assert_equal(s.add_myname_to_mylist(), expected_list)
 
 def test_add_to_mylist():
     """ Verify that we can add a new element to the list """
-    raise SkipTest
-    pass
+    expected_list = [ 'one', 'two', 'three', 'four', 'five', 'new_element']
+    s = SampleClass()
+    assert_equal(s.add_to_mylist('new_element'), expected_list)
